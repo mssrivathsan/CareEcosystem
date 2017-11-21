fa3e096793828b1bd45b9bcb123249e58f90b8b7
Srivathsan MS <mssrivathsan@gmail.com>
2017-11-22 01:12:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/careecosystem/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.careecosystem;
+
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.hide();
+    }
+}
